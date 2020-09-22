2da01546f6f697a7b049449018d950f41f61cff2
Dmitrijv <dmitrij.vel@live.se>
2020-09-22 11:48:04 +0200

    added a user kit from a previous project

--- src/data/UserKit.js
+++ src/data/UserKit.js
@@ -1,29 +1,60 @@
 const ROOT_URL = "https://frebi.willandskill.eu/";
+const API_URL = `${ROOT_URL}api/v1/`;
+const AUTH_URL = `${ROOT_URL}auth/`;
+const LOGIN_URL = `${ROOT_URL}api-token-auth/`;
 
 export default class {
-  async register(
-    firstName,
-    lastName,
-    email,
-    password,
-    organisationName,
-    organisationKind
-  ) {
-    const payload = {
-      firstName,
-      lastName,
-      email,
-      password,
-      organisationName,
-      organisationKind,
-    };
-
-    const url = `${ROOT_URL}auth/users/`;
-
+  async register(firstName, lastName, email, password, organisationName, organisationKind) {
+    const url = `${AUTH_URL}users/`;
+    const payload = { firstName, lastName, email, password, organisationName, organisationKind };
     return fetch(url, {
       method: "POST",
       headers: this.getPublicHeaders(),
-      body: JSON.stringify(payload),
+      body: JSON.stringify(payload)
+    });
+  }
+
+  async activateUser(uid, token) {
+    const url = `${AUTH_URL}users/activate/`;
+    const payload = {
+      uid,
+      token
+    };
+    return fetch(url, {
+      method: "POST",
+      headers: this.getPublicHeaders(),
+      body: JSON.stringify(payload)
+    });
+  }
+
+  async login(email, password) {
+    const url = `${LOGIN_URL}`;
+    const payload = {
+      email,
+      password
+    };
+    return fetch(url, {
+      method: "POST",
+      headers: this.getPublicHeaders(),
+      body: JSON.stringify(payload)
+    });
+  }
+
+  async getCustomerList() {
+    const url = `${API_URL}customers`;
+    //console.log(url);
+    //console.log(this.getPrivateHeaders());
+    return fetch(url, {
+      headers: this.getPrivateHeaders()
+    });
+  }
+
+  async createCustomer(payload) {
+    const url = `${API_URL}customers`;
+    return fetch(url, {
+      method: "POST",
+      headers: this.getPrivateHeaders(),
+      body: JSON.stringify(payload)
     });
   }
 
@@ -32,19 +63,19 @@ export default class {
   }
 
   getToken() {
-    localStorage.getItem("BUSINESS_TOKEN");
+    return localStorage.getItem("BUSINESS_TOKEN");
   }
 
   getPublicHeaders() {
     return {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     };
   }
 
   getPrivateHeaders() {
     return {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${this.getToken()}`,
+      Authorization: `Bearer ${this.getToken()}`
     };
   }
 }
