579f5404617df80c0ef26453f77e0555f20dac7a
Dmitrijv <dmitrij.vel@live.se>
2020-09-22 17:44:23 +0200

    tweak header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import HeaderUserElement from "./HeaderUserElement";
 
 export default function Header() {
   return (
-    <div className="top-header">
+    <div className="header">
       <div className="centered-container">
         <Link to={`/`}>
-          <h1 className="logo">[js3] Dmitrij V. - Exercise 2</h1>
+          <h1 className="logo">Dmitrij V. - Exercise 2</h1>
         </Link>
         <HeaderUserElement />
       </div>
