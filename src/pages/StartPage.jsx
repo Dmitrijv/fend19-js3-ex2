6387451b7024a5f8eeddaba8f2532ef42f3576c9
Dmitrijv <dmitrij.vel@live.se>
2020-09-24 13:29:53 +0200

    remove unused code on start page

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,19 +1,9 @@
-import React, { useContext } from "react";
-import { useHistory, Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import RegisterForm from "./../components/RegisterForm";
 import GlobalLayout from "./layout/GlobalLayout";
 
-import { BusinessContext } from "./../contexts/BusinessContext";
-
 export default function Start() {
-  const history = useHistory();
-  const { userEmail } = useContext(BusinessContext);
-
-  // redirect to home page if we are already logged in
-  if (userEmail) {
-    history.push("/home");
-  }
-
   return (
     <GlobalLayout>
       <div className="white-card">
