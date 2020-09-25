9bb15797c81c7eb4a2322fa8c1cf7f6be2a75b66
Dmitrijv <dmitrij.vel@live.se>
2020-09-25 20:28:11 +0200

    add option to show label tag in InputField component

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import styles from "../styles/js/styles";
 
 export default function InputField({ template }) {
   const placeholder = template.placeholder ? `${template.label} (${template.placeholder})` : template.label;
+
   return (
-    <input
-      placeholder={placeholder}
-      id={template.placeholder || template.label}
-      value={template.value}
-      onChange={e => template.callback(e.target.value)}
-      required={template.required || false}
-      type={template.type || "text"}
-      pattern={template.pattern}
-      minLength={template.minLength}
-      maxLength={template.maxLength}
-    />
+    <styles.InputContainer>
+      {template.showLabel === true && <label>{template.label}</label>}
+      <input
+        placeholder={placeholder}
+        id={template.placeholder || template.label}
+        value={template.value}
+        onChange={e => template.callback(e.target.value)}
+        required={template.required || false}
+        type={template.type || "text"}
+        pattern={template.pattern}
+        minLength={template.minLength}
+        maxLength={template.maxLength}
+      />
+    </styles.InputContainer>
   );
 }
