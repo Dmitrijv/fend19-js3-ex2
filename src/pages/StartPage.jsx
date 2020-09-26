91dc3918b687ae5863282d526dfe7b88afaef482
Dmitrijv <dmitrij.vel@live.se>
2020-09-26 17:54:34 +0200

    switch to styled component

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 import RegisterForm from "./../components/RegisterForm";
 import GlobalLayout from "./layout/GlobalLayout";
 
+import styles from "./../styles/js/styles";
+
 export default function Start() {
   return (
     <GlobalLayout>
-      <div className="white-card">
+      <styles.WhiteCard>
         <RegisterForm />
-      </div>
-      <div className="white-card">
+      </styles.WhiteCard>
+      <styles.WhiteCard>
         <p className="text-center">
           Already registered? <Link to={`/login`}>Sign in here.</Link>
         </p>
-      </div>
+      </styles.WhiteCard>
     </GlobalLayout>
   );
 }
