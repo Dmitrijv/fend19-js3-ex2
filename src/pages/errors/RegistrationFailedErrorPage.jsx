99691359f344049a42585fc903e6728d8a323b65
Dmitrijv <dmitrij.vel@live.se>
2020-09-26 17:57:56 +0200

    change error message wording

--- src/pages/errors/RegistrationFailedErrorPage.jsx
+++ src/pages/errors/RegistrationFailedErrorPage.jsx
@@ -7,7 +7,7 @@ export default function RegistrationFailedErrorPage() {
       <div className="white-card">
         <p className="text-center">Could not register new account.</p>
         <p className="text-center">
-          You have entered incorrect input data or specified email adress is already registered.
+          You have entered incorrect input data or specified an email adress that has already been registered.
         </p>
       </div>
     </GlobalLayout>
