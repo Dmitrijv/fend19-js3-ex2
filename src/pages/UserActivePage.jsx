5a893fe49cff2ad1a5fe03e87e4565f335363b73
Dmitrijv <dmitrij.vel@live.se>
2020-09-26 17:55:32 +0200

    email verification now clears email adress to be verified

--- src/pages/UserActivePage.jsx
+++ src/pages/UserActivePage.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import GlobalLayout from "./layout/GlobalLayout";
 
+import { BusinessContext } from "./../contexts/BusinessContext";
 import heheBusinessImg from "./../image/haha_business.png";
 
 export default function UserActivePage() {
+  const { setEmailToVerify } = useContext(BusinessContext);
+
+  useEffect(() => {
+    setEmailToVerify(null);
+  }, []);
+
   return (
     <GlobalLayout>
       <div className="white-card text-center">
