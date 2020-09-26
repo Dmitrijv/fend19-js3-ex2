896abfbd33dd0a6c3a449e830264cb09aa9a2ed5
Dmitrijv <dmitrij.vel@live.se>
2020-09-26 19:22:01 +0200

    added a custom style sheet to override npm package styling

--- src/components/customer/CustomerTable.jsx
+++ src/components/customer/CustomerTable.jsx
@@ -1,43 +1,22 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import styles from "../../styles/js/styles";
 import CustomerTableRow from "./CustomerTableRow";
 import { Table, Thead, Tbody, Tr, Th } from "react-super-responsive-table";
+
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import "./../../styles/sass/_customer-table-override.scss";
 
 const CustomerSheet = styled(styles.InfoSheet)`
   width: 100%;
-`;
-
-const CustomerTableTag = styled.table`
-  width: 100%;
-  border: none;
-  thead,
-  thead tr {
-    color: white;
-    background-color: #85aa53;
-  }
-
-  tbody tr:hover {
-    background-color: white;
-  }
-
-  tbody td,
-  thead th {
-    padding: 5px;
-  }
-
-  thead th {
-    text-align: left;
-  }
 `;
 
 export default function CustomerTable({ customerList }) {
   return (
     <CustomerSheet>
       <h2>{customerList.length} customer(s) on record</h2>
-      <Table>
+      <Table className="customer-table">
         <Thead>
           <Tr>
             <Th>ID</Th>
